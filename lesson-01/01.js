6ca357ec1a7acb88729a655cca3ce8dd46b455bb
Ripper <zeustianstafford@gmail.com>
2026-04-16 14:26:58 +0300

    init project

---
+++ lesson-01/01.js
@@ -0,0 +1,7 @@
+
+let sum = 0
+let i = 1
+while (i <=20) {
+    sum += i;
+    i++;
+}
---
+++ lesson-01/03.js
@@ -0,0 +1,11 @@
+let sum = 0;
+let i = 1;
+
+while (i <= 20) {
+    if (i % 2 !== 0) {
+        sum += i;
+    }
+    i++;
+}
+
+console.log(sum); 

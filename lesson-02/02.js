0666367a587a97defe39ad9e53dada898c11b940
Ripper <zeustianstafford@gmail.com>
2026-04-23 13:47:03 +0300

    NewLess

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -5,7 +5,7 @@
 
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
-
+function rollDice(object){
 let dice1 = Math.floor(Math.random() * 6) + 1;
 let dice2 = Math.floor(Math.random() * 6) + 1;
 let isWinningDouble = false;
@@ -17,8 +17,15 @@ if (dice1 === dice2 && dice1 >= 3) {
 console.log(isWinningDouble, dice1, dice2)
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-
+// if (isWinningDouble) {
+//   console.log('Выигрышный дубль!')
+// } else {
+//   console.log('Не выигрышный дубль.')
+// }
 if (!isWinningDouble){
   return ('Проигрышный дубль.')
 }
 return ('Выигрышный дубль.')
+}
+
+console.log(rollDice());

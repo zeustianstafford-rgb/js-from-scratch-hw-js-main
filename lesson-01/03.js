f52e8a0e6129c9882c03492e27439be53995c98f
Ripper <zeustianstafford@gmail.com>
2026-04-23 21:31:55 +0300

    moreTryTwoo

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,11 +1,11 @@
-let sum = 0;
-let i = 1;
+// let sum = 0;
+// let i = 1;
 
-while (i <= 20) {
-    if (i % 2 !== 0) {
-        sum += i;
-    }
-    i++;
-}
+// while (i <= 20) {
+//     if (i % 2 !== 0) {
+//         sum += i;
+//     }
+//     i++;
+// }
 
-console.log(sum); 
+// console.log(sum); 
--- lesson-02/02.js
+++ lesson-02/02.js
@@ -1,31 +1,31 @@
-/*
-* Напишите код, который симулирует бросок двух шестигранных игральных кубиков и определяет, выпал ли выигрышный дубль.
-* Дубль считается выигрышным, если на обоих дайсах выпало одно и то же число, и это число больше 3.
-* Полученное булево значение сохраните в переменной isWinningDouble
+// /*
+// * Напишите код, который симулирует бросок двух шестигранных игральных кубиков и определяет, выпал ли выигрышный дубль.
+// * Дубль считается выигрышным, если на обоих дайсах выпало одно и то же число, и это число больше 3.
+// * Полученное булево значение сохраните в переменной isWinningDouble
 
-* Для успешного прохождения тестов не меняйте названия переменных!
-*/
-function rollDice(object){
-let dice1 = Math.floor(Math.random() * 6) + 1;
-let dice2 = Math.floor(Math.random() * 6) + 1;
-let isWinningDouble = false;
+// * Для успешного прохождения тестов не меняйте названия переменных!
+// */
+// function rollDice(object){
+// let dice1 = Math.floor(Math.random() * 6) + 1;
+// let dice2 = Math.floor(Math.random() * 6) + 1;
+// let isWinningDouble = false;
 
 
-if (dice1 === dice2 && dice1 >= 3) {
-  isWinningDouble = true
-};
-console.log(isWinningDouble, dice1, dice2)
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-// if (isWinningDouble) {
-//   console.log('Выигрышный дубль!')
-// } else {
-//   console.log('Не выигрышный дубль.')
+// if (dice1 === dice2 && dice1 >= 3) {
+//   isWinningDouble = true
+// };
+// console.log(isWinningDouble, dice1, dice2)
+// console.log('Первый бросок: ' + dice1)
+// console.log('Второй бросок: ' + dice2)
+// // if (isWinningDouble) {
+// //   console.log('Выигрышный дубль!')
+// // } else {
+// //   console.log('Не выигрышный дубль.')
+// // }
+// if (!isWinningDouble){
+//   return ('Проигрышный дубль.')
 // }
-if (!isWinningDouble){
-  return ('Проигрышный дубль.')
-}
-return ('Выигрышный дубль.')
-}
+// return ('Выигрышный дубль.')
+// }
 
-console.log(rollDice());
+// console.log(rollDice());

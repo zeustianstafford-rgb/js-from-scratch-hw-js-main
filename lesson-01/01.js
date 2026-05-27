216e9555ef8e124e0a461b0ec048d5dfcc1dbc21
Ripper <zeustianstafford@gmail.com>
2026-05-27 22:29:14 +0300

    1

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -10,3 +10,4 @@ while (i <= 20) {
     i++;
 };
 console.log(sum);
+
--- lesson-02/01.js
+++ lesson-02/01.js
@@ -6,17 +6,17 @@
 * - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
 * - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
 */
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+// const isAdmin = false
+// const isVerifiedUser = true
+// const hasSpecialPermission = true
+// const hasTemporaryPass = false
 
 
 
-// your code
-const A = isAdmin || isVerifiedUser
-const B = hasSpecialPermission || hasTemporaryPass
-let isAccess = A &&  B
+// // your code
+// const A = isAdmin || isVerifiedUser
+// const B = hasSpecialPermission || hasTemporaryPass
+// let isAccess = A && B
 // тестовые данные (значения можно менять)
 
 // const isAdmin = false;
@@ -58,7 +58,7 @@ let isAccess = A &&  B
 
 // }
 // else {
-//     alert("Доступ запрещен!")
+//     a("Доступ запрещен!")
 // };
 // console.log(isAccess);
 
@@ -95,3 +95,30 @@ let isAccess = A &&  B
 // }
 // console.log(getResourse(user));
 // console.log(getResourse(pechkin));
+
+
+
+const isAdmin = false
+const isVerifiedUser = true
+const hasSpecialPermission = true
+const hasTemporaryPass = false
+
+let isAccess
+
+// your code 
+// Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
+// * Полученное булево значение сохраните в переменной isAccess.
+
+// * Условия доступа:
+// * - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
+// * - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
+
+// (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)
+
+if (isAdmin || isVerifiedUser && hasSpecialPermission  || hasTemporaryPass) {
+    isAccess = true
+    console.log('Доступ разрешен')
+} else {
+    console.log('Accesse denied permission')
+    isAccess = false;
+};
